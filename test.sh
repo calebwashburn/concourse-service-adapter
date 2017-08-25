91362a8e225f41da78f4b30b395434541728a65a
datianshi <dsz0111@gmail.com>
2017-08-25 18:59:55 -0500

    add an integration test script

---
+++ test.sh
@@ -0,0 +1,89 @@
+service_deployment_json=$(jq -n '
+{
+    "deployment_name": "service-instance_abcd",
+    "releases": [{
+        "name": "concourse",
+        "version": "0.1.2",
+        "jobs": [
+            "atc",
+            "tsa",
+            "baggageclaim",
+            "groundcrew",
+            "postgresql"
+        ]
+    },
+    {
+        "name": "garden-runc",
+        "version": "0.1.3",
+        "jobs": [
+            "garden"
+        ]
+    }
+    ],
+    "stemcell": {
+        "stemcell_os": "BeOS",
+        "stemcell_version": "2"
+    }
+}
+')
+
+plan_json='
+{
+   "instance_groups": [
+      {
+         "name": "web",
+         "vm_type": "small",
+         "networks": [
+            "example-network"
+         ],
+         "azs": [
+            "example-az"
+         ],
+         "instances": 1
+      },
+      {
+         "name": "db",
+         "vm_type": "small",
+         "networks": [
+            "example-network"
+         ],
+         "azs": [
+            "example-az"
+         ],
+         "instances": 1,
+         "persistent_disk_type": "ten"
+      },
+      {
+         "name": "worker",
+         "vm_type": "small",
+         "networks": [
+            "example-network"
+         ],
+         "azs": [
+            "example-az"
+         ],
+         "instances": 1
+      }
+   ],
+   "properties": {
+      "example": "property"
+   },
+   "update": {
+      "canaries": 1,
+      "max_in_flight": 2,
+      "canary_watch_time": "1000-30000",
+      "update_watch_time": "1000-30000",
+      "serial": true
+  }
+}
+'
+
+request_params_json='
+{
+  "parameters": {
+    "app_domain": "appsdomain.com"
+  }
+}
+'
+
+./main generate-manifest "$service_deployment_json" "$plan_json" "$request_params_json" "---" "{}"
